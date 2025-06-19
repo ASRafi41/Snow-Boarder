ccbdb7af3b50babbd2957e44a183e94a96ea7e4a
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2025-06-19 19:31:41 +0600

    40 Done

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayBeforeReload = 0.5f; // Delay before reloading the scene
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Ground"))
         {
-           Debug.Log("Dead!");
+            Debug.Log("Dead!");
+            crashEffect.Play(); // Play crash effect
+            Invoke("reloadScene", delayBeforeReload); // Delay to allow player to see the message
             // Assuming there's a GameManager that handles game state
             // GameManager.Instance.OnPlayerCrash();
         }
+    }
+    void reloadScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -1,12 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayBeforeReload = 1f; // Delay before reloading the scene
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Finish Line!");
+            finishEffect.Play(); // Play finish effect
+            Invoke("reloadScene", delayBeforeReload); // Delay to allow player to see the message
         }
+    }
+    
+    void reloadScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
